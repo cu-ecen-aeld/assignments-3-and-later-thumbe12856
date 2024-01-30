320ef7dd310288a9834ad7f720567346e8bd106f
thumbe12856 <thumbd12856@gmail.com>
2024-01-30 18:45:14 +0800

    fork, exec, waitpid

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -7,17 +7,20 @@
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
 */
-bool do_system(const char *cmd)
+bool do_system(const char* cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+    /*
+     *  Call the system() function with the command set in the cmd
+     *   and return a boolean true if the system() call completed with success
+     *   or false() if it returned a failure
+    */
+    int status = system(cmd);
+    if (status == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 /**
@@ -33,34 +36,91 @@ bool do_system(const char *cmd)
 *   fork, waitpid, or execv() command, or if a non-zero return value was returned
 *   by the command issued in @param arguments with the specified arguments.
 */
-
 bool do_exec(int count, ...)
 {
+    openlog("do_exec", 0, LOG_USER);
+
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char* command[count + 1];
     int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
+    for (i = 0; i < count; i++) {
+        command[i] = va_arg(args, char*);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    char command_binary[1024];
+    strcpy(command_binary, command[0]);
+    if (command_binary[0] != '/') {
+        // The command binary is not specified.
+        return false;
+    }
 
+    // Find the last occurrence of the directory separator '/'
+    char* filename;
+    filename = strrchr(command[0], '/');
+    if (filename != NULL) {
+        filename += 1;
+        command[0] = filename;
+    }
+
+    char command_str[1024];
+    int offset = 0;
+    // Concatenate the command and arguments into a single string
+    for (i = 0; i < count + 1; i++) {
+        offset += sprintf(command_str + offset, "%s ", command[i]);
+    }
+
+    /*
+     *   Execute a system command by calling fork, execv(),
+     *   and wait instead of system (see LSP page 161).
+     *   Use the command[0] as the full path to the command to execute
+     *   (first argument to execv), and use the remaining arguments
+     *   as second argument to the execv() command.
+     *
+    */
+    int status;
+    syslog(LOG_INFO, "Start forking with the command: %s, \"%s\"\n", command_binary, command_str);
+    fflush(stdout);
+    pid_t pid = fork();
+    if (pid == -1) {
+        // Fail to fork.
+        syslog(LOG_ERR, "Fail to fork with the command: %s, \"%s\"\n", command_binary, command_str);
+        va_end(args);
+        return false;
+    }
+    else if (pid == 0) {
+        // Child process
+        execv(command_binary, command);
+
+        syslog(LOG_ERR, "Args: %s, \"%s\". pid = %d\n", command_binary, command_str, pid);
+        // execv returns if and only if there is an error.
+        va_end(args);
+        return false;
+    }
+    else if (waitpid(pid, &status, 0) == -1) {
+        // Error occurred while waiting for child process.
+        syslog(LOG_ERR, "waitpid fails with the command: %s, \"%s\".\n", command_binary, command_str);
+        perror("waitpid");
+        va_end(args);
+        return false;
+    }
+    else if (WIFEXITED(status)) {
+        // Successfully executed the command.
+        syslog(LOG_USER, "Successfully executed the command: %s, \"%s\". status = %i, WEXITSTATUS(status) = %i. pid = %d\n", command_binary, command_str, status, WEXITSTATUS(status), pid);
+        va_end(args);
+        return !WEXITSTATUS(status);
+    }
+    else {
+        // Command executed failed.
+        syslog(LOG_ERR, "Command executed failed with the command: %s, \"%s\".\n", command_binary, command_str);
+        va_end(args);
+        return false;
+    }
+
+    syslog(LOG_INFO, "return true: %s, \"%s\".\n", command_binary, command_str);
+    fflush(stdout);
     va_end(args);
-
     return true;
 }
 
@@ -69,15 +129,15 @@ bool do_exec(int count, ...)
 *   This file will be closed at completion of the function call.
 * All other parameters, see do_exec above
 */
-bool do_exec_redirect(const char *outputfile, int count, ...)
+bool do_exec_redirect(const char* outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char* command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
-        command[i] = va_arg(args, char *);
+        command[i] = va_arg(args, char*);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
@@ -85,15 +145,68 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = command[count];
 
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    /*
+     *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+     *   redirect standard out to a file specified by outputfile.
+     *   The rest of the behaviour is same as do_exec()
+     *
+    */
+    int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if (fd < 0)
+    {
+        syslog(LOG_ERR, "failed to open the file: %s.", outputfile);
+        va_end(args);
+        return false;
+    }
+    fflush(stdout);
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        // Fail to fork.
+        syslog(LOG_ERR, "Fail to fork.");
+        va_end(args);
+        return false;
+    }
+    else if (pid == 0) {
+        // Child process
+        if (dup2(fd, 1) < 0) {
+            syslog(LOG_ERR, "Fail to dup2.");
+            close(fd);
+            va_end(args);
+            return false;
+        }
+
+        close(fd);
+        execvp(command[0], command);
+
+        syslog(LOG_ERR, "execvp failed\n");
+        // execvp returns if and only if there is an error.
+        va_end(args);
+        return false;
+    }
+    else {
+        close(fd);
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            // Error occurred while waiting for child process.
+            syslog(LOG_ERR, "waitpid fails.\n");
+            va_end(args);
+            return false;
+        }
+        else if (WIFEXITED(status)) {
+            // Successfully executed the command.
+            va_end(args);
+            syslog(LOG_USER, "Successfully executed the command.\n");
+            return !WEXITSTATUS(status);;
+        }
+        else {
+            // Command execution failed
+            syslog(LOG_ERR, "Command executed failed.\n");
+            va_end(args);
+            return false;
+        }
+    }
 
     va_end(args);
-
     return true;
 }
--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <string.h>
 
-bool do_system(const char *command);
+bool do_system(const char* command);
 
 bool do_exec(int count, ...);
 
-bool do_exec_redirect(const char *outputfile, int count, ...);
+bool do_exec_redirect(const char* outputfile, int count, ...);
